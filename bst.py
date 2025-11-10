4fb1d10c67bfa8ec6f8538fb5ebbebe7ed9a6410
Ori Azar <oriazarpersonal@gmail.com>
2025-11-09 16:59:00 -0800

    Update bst.py

--- bst.py
+++ bst.py
@@ -1,1 +1,14 @@
 
+import sys
+import unittest
+from typing import *
+from dataclasses import dataclass
+sys.setrecursionlimit(10**6)
+
+
+BSTree : TypeAlias = Union[None, "BSTNode"]
+
+class BSTNode:
+    val =  int 
+    left : BSTree
+    right : BSTree
